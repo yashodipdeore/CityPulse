6eabc570435a6c7340bc25e39eb5fa921dd76498
yashodipdeore <yashodipdeore2006@gmail.com>
2025-08-22 09:28:22 +0530

    nice

--- frontend/cityplus/src/main.jsx
+++ frontend/cityplus/src/main.jsx
@@ -1,5 +1,7 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
+//import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import App from "./App.jsx";
 import "./index.css";
